4076b632d21538e6a02e31fde7181fc766b06214
navrkr <68466584+navrkr@users.noreply.github.com>
2022-11-28 22:51:21 +0530

    Add files via upload

--- js/main2.js
+++ js/main2.js
@@ -18,6 +18,18 @@ $(document) .ready(function() {
           }
       }
      });
+ 
+     $(document).ready(function() {
+
+      $('.color-choose input').on('click', function() {
+          var headphonesColor = $(this).attr('data-image');
+    
+          $('.active').removeClass('active');
+          $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
+          $(this).addClass('active');
+      });
+    
+    });
     
     var swiper = new Swiper('.swiper', {
       autoplay: {
@@ -95,6 +107,36 @@ $(document) .ready(function() {
  
  }); 
  });
+
+ let slideIndex = 1;
+ showSlides(slideIndex);
+ 
+ function plusSlides(n) {
+   showSlides(slideIndex += n);
+ }
+ 
+ function currentSlide(n) {
+   showSlides(slideIndex = n);
+ }
+ 
+ function showSlides(n) {
+   let i;
+   let slides = document.getElementsByClassName("mySlides");
+   let dots = document.getElementsByClassName("demo");
+   let captionText = document.getElementById("caption");
+   if (n > slides.length) {slideIndex = 1}
+   if (n < 1) {slideIndex = slides.length}
+   for (i = 0; i < slides.length; i++) {
+     slides[i].style.display = "none";
+   }
+   for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" active", "");
+   }
+   slides[slideIndex-1].style.display = "block";
+   dots[slideIndex-1].className += " active";
+   captionText.innerHTML = dots[slideIndex-1].alt;
+ }
+
  
   /*======================================
      Google Map
